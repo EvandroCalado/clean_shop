27d5863520232b3401a5fdc10554ddec447bf6a7
Evandro Calado <evandrocalado07@gmail.com>
2026-08-17 19:52:40 -0300

    feat: implement save, findById, and findAll methods in DrizzleProductRepository; add toCents method in Money value object

--- src/product/infrastructure/adapters/drizzle-product.repository.ts
+++ src/product/infrastructure/adapters/drizzle-product.repository.ts
@@ -9,18 +9,46 @@ import {
   DRIZZLE,
   type DrizzleDB,
 } from 'src/shared/infrastructure/database/postgres/drizzle.provider';
+import { products } from 'src/shared/infrastructure/database/postgres/schema';
 
 @Injectable()
 export class DrizzleProductRepository implements ProductRepositoty {
   constructor(@Inject(DRIZZLE) private readonly db: DrizzleDB) {}
 
   async save(procuct: Product): Promise<void> {
+    const row = DrizzleProductRepository.toPersistence(procuct);
+    await this.db
+      .insert(products)
+      .values(row)
+      .onConflictDoUpdate({
+        target: products.id,
+        set: {
+          ...row,
+        },
+      });
+  }
+
+  async findById(id: ProductId): Promise<Product | null> {
     throw new Error('Method not implemented.');
   }
-  findById(id: ProductId): Promise<Product | null> {
+
+  async findAll(filters: ProductFilters): Promise<Product[]> {
     throw new Error('Method not implemented.');
   }
-  findAll(filters: ProductFilters): Promise<Product[]> {
-    throw new Error('Method not implemented.');
+
+  private static toPersistence(product: Product): typeof products.$inferSelect {
+    return {
+      id: product.id.getValue(),
+      name: product.name,
+      description: product.description,
+      priceAmount: product.price.getAmount(),
+      priceCurrency: product.price.getCurrency(),
+      sku: product.sku.getValue(),
+      stock: product.stock,
+      isActive: product.isActive,
+      lowStockThreshold: product.lowStockThreshold,
+      createdAt: product.createdAt,
+      updatedAt: product.updatedAt,
+    };
   }
 }
